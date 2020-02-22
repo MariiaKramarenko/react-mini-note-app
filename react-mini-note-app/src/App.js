1be180431945749d941417b11468ff3e76a62bd7
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-02-22 20:10:52 +0100

    added dependencies(libraries) and used create-react-app, added main page Home.js

---
+++ react-mini-note-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+function App() {
+  return (
+  	<BrowserRouter>
+  	   <Switch>
+           <Route />
+    <div className="container pt-4">
+     <h1>Test</h1>
+    </div>
+       </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
