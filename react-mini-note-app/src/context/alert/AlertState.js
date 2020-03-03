1fb3ab0727cb752ebc2b698b3fef56a38aa090ff
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-03-03 16:24:31 +0100

    created reducer and state for alert

--- react-mini-note-app/src/context/alert/AlertState.js
+++ react-mini-note-app/src/context/alert/AlertState.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useReducer} from 'react';//{useReducer} -хук для использования редьюсера
 import {AlertContext} from './alertContext';
+import {SHOW_ALERT} from "../types";
+import {HIDE_ALERT} from "../types";
+import {alertReducer} from "./alertReducer";
 
 export const AlertState = ({children}) =>{//контекст вызова для работы с алертом
+	const [state, dispatch] = useReducer(alertReducer, {visible: false})//созд массив и указываем что исп редьюсер alertReducer, второй параметр-initial state
+	
+    const show = (text, type = "warning") =>{
+    	dispatch({
+    		type: SHOW_ALERT,
+    		payload: {text, type}
+    	})
+    }
+
+    const hide = ()=> dispatch({type: HIDE_ALERT})
+
+
 	return(
-		<AlertContext.Provider>
+		<AlertContext.Provider value={{show, hide, alert: state}}>
 			{children}
 		</AlertContext.Provider>
 	)
---
+++ react-mini-note-app/src/context/types.js
@@ -0,0 +1,2 @@
+export const SHOW_ALERT = 'SHOW_ALERT';//тут создаем экшны для редьюсера alertReducer
+export const HIDE_ALERT = 'HIDE_ALERT';
